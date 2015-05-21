3a2a39085636d38d001efce3bb41f3caba37d310
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2015-05-21 11:37:51 -0700

    Updating samples to use application default credentials.
    Updating samples to pass lint and making them slightly more readable.

--- python-example/README.md
+++ python-example/README.md
@@ -1,10 +1,27 @@
-Python Samples
-============
+## Python Samples for Google Cloud Storage
 
 Two samples:
 
-- storage-sample.py - lists objects in a bucket.
-- componse-sample.py - composes two objects together to create another.
+1. ``storage-sample.py`` lists objects in a bucket.
+2. ``componse-sample.py`` composes objects together to create another.
 
-For more information details about running these samples, including how to create the client_secrets.json file, see
-https://developers.google.com/storage/docs/json_api/v1/json-api-python-samples.
+See the docstring for each sample for usage, or run the sample for the help text.
+
+### Setup
+
+Before running the samples, you'll need the Google Cloud SDK in order to setup authentication.
+
+1. Install the [Google Cloud SDK](https://cloud.google.com/sdk/), including the [gcloud tool](https://cloud.google.com/sdk/gcloud/), and [gcloud app component](https://cloud.google.com/sdk/gcloud-app).
+2. Setup the gcloud tool.
+
+   ```
+   gcloud components update app
+   gcloud auth login
+   gcloud config set project <your-app-id>
+   ```
+
+You will also need to install the dependencies using [pip](https://pypi.python.org/pypi/pip):
+
+```
+pip install -r requirements.txt
+```
--- python-example/storage-sample.py
+++ python-example/storage-sample.py
@@ -14,95 +14,64 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # [START all]
-"""Command-line skeleton application for Cloud Storage API.
+"""Command-line sample application for listing all objects
+in a bucket using the Cloud Storage API.
+
+Before running, authenticate with the Google Cloud SDK by running:
+  $ gcloud auth login
+
 Usage:
-  $ python storage-sample.py
+  $ python storage-sample.py <your-bucket>
 
 You can also get help on all the command-line flags the program understands
 by running:
-
   $ python storage-sample.py --help
 
 """
 
 import argparse
-import httplib2
-import os
 import sys
 import json
 
 from apiclient import discovery
-from oauth2client import file
-from oauth2client import client
-from oauth2client import tools
+from oauth2client.client import GoogleCredentials
 
-# Define sample variables.
-_BUCKET_NAME = '[[INSERT_YOUR_BUCKET_NAME_HERE]]'
-_API_VERSION = 'v1'
 
 # Parser for command-line arguments.
 parser = argparse.ArgumentParser(
     description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[tools.argparser])
-
-
-# CLIENT_SECRETS is name of a file containing the OAuth 2.0 information for this
-# application, including client_id and client_secret. You can see the Client ID
-# and Client secret on the APIs page in the Cloud Console:
-# <https://console.developers.google.com/>
-CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
-
-# Set up a Flow object to be used for authentication.
-# Add one or more of the following scopes. PLEASE ONLY ADD THE SCOPES YOU
-# NEED. For more information on using scopes please see
-# <https://developers.google.com/storage/docs/authentication#oauth>.
-FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS,
-  scope=[
-      'https://www.googleapis.com/auth/devstorage.full_control',
-      'https://www.googleapis.com/auth/devstorage.read_only',
-      'https://www.googleapis.com/auth/devstorage.read_write',
-    ],
-    message=tools.message_if_missing(CLIENT_SECRETS))
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('bucket')
 
 
 def main(argv):
   # Parse the command-line flags.
-  flags = parser.parse_args(argv[1:])
+  args = parser.parse_args(argv[1:])
 
-  # If the credentials don't exist or are invalid run through the native client
-  # flow. The Storage object will ensure that if successful the good
-  # credentials will get written back to the file.
-  storage = file.Storage('sample.dat')
-  credentials = storage.get()
-  if credentials is None or credentials.invalid:
-    credentials = tools.run_flow(FLOW, storage, flags)
+  # Get the application default credentials. When running locally, these are
+  # available after running `gcloud auth login`. When running on compute
+  # engine, these are available from the environment.
+  credentials = GoogleCredentials.get_application_default()
 
-  # Create an httplib2.Http object to handle our HTTP requests and authorize it
-  # with our good Credentials.
-  http = httplib2.Http()
-  http = credentials.authorize(http)
+  # Construct the service object for interacting with the Cloud Storage API.
+  service = discovery.build('storage', 'v1', credentials=credentials)
 
-  # Construct the service object for the interacting with the Cloud Storage API.
-  service = discovery.build('storage', _API_VERSION, http=http)
+  # Make a request to buckets.get to retrieve information about the bucket.
+  req = service.buckets().get(bucket=args.bucket)
+  resp = req.execute()
+  print json.dumps(resp, indent=2)
 
-  try:
-    req = service.buckets().get(bucket=_BUCKET_NAME)
+  # Create a request to objects.list to retrieve a list of objects.
+  fields_to_return = \
+      'nextPageToken,items(name,size,contentType,metadata(my-key))'
+  req = service.objects().list(bucket=args.bucket, fields=fields_to_return)
+
+  # If you have too many items to list in one request, list_next() will
+  # automatically handle paging with the pageToken.
+  while req is not None:
     resp = req.execute()
     print json.dumps(resp, indent=2)
-
-    fields_to_return = 'nextPageToken,items(name,size,contentType,metadata(my-key))'
-    req = service.objects().list(bucket=_BUCKET_NAME, fields=fields_to_return)
-    # If you have too many items to list in one request, list_next() will
-    # automatically handle paging with the pageToken.
-    while req is not None:
-      resp = req.execute()
-      print json.dumps(resp, indent=2)
-      req = service.objects().list_next(req, resp)
-
-  except client.AccessTokenRefreshError:
-    print ("The credentials have been revoked or expired, please re-run"
-      "the application to re-authorize")
+    req = service.objects().list_next(req, resp)
 
 if __name__ == '__main__':
   main(sys.argv)
